56a88ff7a9970b0c39a344cbbb6dc6bad1804f99
Matt Hink <mhink1103@gmail.com>
2011-12-04 16:46:29 -0600

    Removed unnecessary BNPriorityQueue class (Now using TreeSet)

--- src/edu/msstate/cse/mrh208/Algorithms/AStar.java
+++ src/edu/msstate/cse/mrh208/Algorithms/AStar.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.TreeSet;
 
-import edu.msstate.cse.mrh208.BNPriorityQueue;
-
 import edu.msstate.cse.mrh208.Bayes.BayesianNetwork;
 
 
